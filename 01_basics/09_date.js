733c185b9c30616c310e1a5f810ec1aabcfbb6a5
VISHAL KUSHAWAH <kushawahavishal29@gmail.com>
2026-08-19 12:20:01 +0530

    date file

---
+++ 01_basics/09_date.js
@@ -0,0 +1,17 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+
+// let myCreatedDAte = new Date(2026,0,25)
+// let myCreatedDAte = new Date(2026,0,25,5,30,0)
+// let myCreatedDAte = new Date("2026-01-14");
+let myCreatedDAte = new Date("01-14-2026");
+// console.log(myCreatedDAte.toDateString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDAte.getTime());
+console.log(Math.floor(Date.now()/1000));
