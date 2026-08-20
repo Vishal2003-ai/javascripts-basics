947844f6ec3155dec8ec26db42be4f1a4ccba434
vishal <kushawahavishal29@gmail.com>
2026-08-20 09:57:01 +0530

    newco

---
+++ 03_introjs/script.js
@@ -0,0 +1,12 @@
+// All Datatypes in javascripts
+let num = 3.14// number
+let num2 = 10
+
+console.log(num + num2);
+// string
+const firstName = "vishal"
+const lastName = "kushawah"
+console.log(firstName +" " + lastName);
+
+
+//boolean
---
+++ 04_practise/01_text.js
@@ -0,0 +1,6 @@
+const num1 = 80
+const num2 = 5
+
+const oprt = "*"
+const result = num1 * num2
+console.log(`The result of ${num1} ${oprt} ${num2} = ${result}`)
